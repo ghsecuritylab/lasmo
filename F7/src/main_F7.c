a26e99104e9b8850f568de946eaa6226cac5b762
Luc Moussiegt <luc.moussiegt@telecom-paristech.fr>
2018-12-11 13:02:55 +0100

    A simple 'Hello World' page is sent when a connection is made on the Ethernet

--- F7/src/main_F7.c
+++ F7/src/main_F7.c
@@ -1,9 +1,10 @@
 #include "ch.h"
 #include "hal.h"
+
 #include "RTT/SEGGER_RTT.h"
 #include "RTT/SEGGER_RTT_Conf.h"
-#include "F7/shell_rtt.h"
 
+#include "F7/shell_rtt.h"
 #include "F7/adc.h"
 #include "F7/galva.h"
 #include "F7/sd.h"
@@ -11,6 +12,7 @@
 #include "F7/decoder.h"
 #include "F7/uart.h"
 #include "F7/converter.h"
+#include "F7/eth_F7.h"
 
 int main(void) {
   halInit();
@@ -20,33 +22,10 @@ int main(void) {
   // RTT configuration
   SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
 
-  // lsm_shell_rtt_init();
-
-  //lsm_galva_test();
-
-  lsm_sd_init();
-
-
-  //lsm_adc_init();
-
-  lsm_converter_init("ILDA_files/tunnel/tunnelride2.ild");
-  lsm_converter_start();
-  /* lsm_converter_start(); */
-
-  /* lsm_max5105_init(); */
-  /* lasers_sd_mute(1, 1); */
-  /* lsm_lasers_test(); */
-
-	//lsm_lasers_init();
-	//lsm_lasers_test();
-	//lsm_sd_init();
-
-  lsm_uart_init();
-	lsm_uart_rx();
-
   while(1){
     chThdSleepMilliseconds(10);
     /* if(fs_ready == 0) */
       /* lsm_sd_test(); */
   }
+
 }
