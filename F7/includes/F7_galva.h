fe6b7d79db8e53bfb452651ca8cec3df4d7daf14
Aurelie <aurelie.marinho@telecom-paristech.fr>
2018-12-14 15:51:56 +0100

    test with channel 2 but doesn't work

--- F7/includes/F7_galva.h
+++
@@ -1,7 +0,0 @@
-#ifndef F7_GALVA_H
-#define F7_GALVA_H
-
-void lsm_control_galvaX(uint32_t x_to);
-void lsm_control_galvaY(uint32_t y_to);
-
-#endif
---
+++ F7/src/main.c
@@ -0,0 +1,27 @@
+#include "ch.h"
+#include "hal.h"
+#include "RTT/SEGGER_RTT.h"
+#include "RTT/SEGGER_RTT_Conf.h"
+#include "F7/galva.h"
+
+int main(void) {
+  halInit();
+  chSysInit();
+  SEGGER_RTT_printf(0, "Start ! \r\n");
+
+  // RTT configuration
+  SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
+  SEGGER_RTT_printf(0, "avant init ! \r\n");
+  lsm_galva_init();
+  SEGGER_RTT_printf(0, "apres init ! \r\n");
+  lsm_ctrl_galvaXY(0,0);
+  SEGGER_RTT_printf(0, "1! \r\n");
+
+  lsm_ctrl_galvaXY(2048,2048);
+  SEGGER_RTT_printf(0, "2 ! \r\n");
+
+  lsm_ctrl_galvaXY(4095,4095);
+  while(1){
+   chThdSleepMilliseconds(10);
+  }
+}
