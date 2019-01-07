d28d30e4a5f828d2030bbe85d3d02e55686aa184
Aurélie Marinho <aurelie.marinho@telecom-paristech.fr>
2019-01-07 17:18:56 +0100

    F7 control galva

---
+++ F7/includes/F7/galva.h
@@ -0,0 +1,16 @@
+#ifndef GALVA_H
+#define GALVA_H
+
+#define AXE_X     0
+#define AXE_Y     1
+#define CHANNEL1  0U
+#define CHANNEL2  1U
+
+void lsm_galva_init(void);
+void lsm_ctrl_galva(int axe, uint16_t value );
+void lsm_ctrl_galvaXY(int16_t x_value, uint16_t y_value);
+void lsm_sin_ctrl_galva(void);
+void lsm_print_line(int16_t x_value);
+void lsm_galva_test(void);
+
+#endif
